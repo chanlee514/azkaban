8abb43b10808f459e276bfd497feffca21ad924e
Chan Lee <chanlee514@gmail.com>
2017-11-13 19:28:40 -0800

    ...

--- azkaban-execserver/src/main/java/azkaban/execapp/event/StatusEventListener.java
+++ azkaban-execserver/src/main/java/azkaban/execapp/event/StatusEventListener.java
@@ -5,12 +5,14 @@ import azkaban.event.EventListener;
 import azkaban.execapp.JobRunner;
 import azkaban.executor.Status;
 import azkaban.utils.Props;
+import azkaban.utils.RestfulApiClient;
 import org.apache.log4j.Logger;
 import status.Types;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import com.google.gson.JsonObject;
 
 import static azkaban.executor.Status.SUCCEEDED;
 
@@ -23,6 +25,10 @@ public class StatusEventListener implements EventListener {
     private static final String STATUS_TENANT = "status.tenant";
     private static final String STATUS_TYPE = "status.type";
     private static final String STATUS_ENVIRONMENT = "status.environment";
+
+    private static final String EVENT_STATUS_PENDING = "PENDING";
+    private static final String EVENT_STATUS_SUCCESSFUL = "SUCCESSFUL";
+    private static final String EVENT_STATUS_FAILURE = "FAILURE";
 
     @Override
     public void handleEvent(Event event) {
@@ -74,65 +80,56 @@ public class StatusEventListener implements EventListener {
 
     }
 
-    /**
-     * Gets tags.
-     *
-     * @return the tags
-     */
-    public Map<String, String> getTags(Logger logger, JobRunner runner) {
-        Map<String, String> tags = new HashMap<String, String>();
+    private void alertJobStarted(
+            Logger logger,
+            JobRunner runner,
+            Event event) throws Exception {
 
-        Props azkProps = runner.getProps();
+        alert(logger, runner, event, EVENT_STATUS_PENDING);
+    }
 
-        if (azkProps != null) {
-            for (String key : azkProps.getKeySet()) {
-                String value = azkProps.get(key);
-                if (key.startsWith("status.tag.")) {
-                    String tag = key.substring("status.tag.".length());
-                    tags.put(tag, value);
-                } else {
-                    tags.put("azkaban.props." + key, value);
-                }
-            }
+    private void alertJobFinished(
+            Logger logger,
+            JobRunner runner,
+            Event event) throws Exception {
+
+        if (Status.isStatusFinished(runner.getStatus()) &&
+            !runner.getStatus().equals(SUCCEEDED)) {
+
+            alert(logger, runner, event, EVENT_STATUS_FAILURE);
+        } else {
+            alert(logger, runner, event, EVENT_STATUS_SUCCESSFUL);
         }
-
-        return tags;
     }
 
-    private void alertJobStarted(Logger logger, JobRunner runner, Event event) throws Exception {
-        Types.Event.Status status = Types.Event.Status.PENDING;
+    public void alert(
+            Logger logger,
+            JobRunner runner,
+            Event event,
+            String status) throws Exception {
 
-        alert(logger, runner, event, status);
-    }
-
-    private void alertJobFinished(Logger logger, JobRunner runner, Event event) throws Exception {
-        Types.Event.Status status = Types.Event.Status.SUCCESSFUL;
-        if (Status.isStatusFinished(runner.getStatus()) && !runner.getStatus().equals(SUCCEEDED)) status = Types.Event.Status.FAILURE;
-
-        alert(logger, runner, event, status);
-    }
-
-    public void alert(Logger logger, JobRunner runner, Event event, Types.Event.Status status) throws Exception {
-        Map<String, String> tags = getTags(logger, runner);
-
-        Types.Event.TenantId tenantId = getTenant(runner);
-        Types.Event.Type type = getType(runner);
+        Map<String, String> tags = this.getTags(logger, runner);
+        if (tags == null) tags = Collections.emptyMap();
 
         Map<String, String> results = Collections.emptyMap();
+
+        String tenantId = this.getTenant(runner);
+        String type = this.getType(runner);
 
         sendEvent(logger, runner, tenantId, type, tags, results, status);
     }
 
-    private Types.EventReceivedResponse sendEvent(
+    private JsonObject sendEvent(
             Logger logger,
             JobRunner runner,
-            Types.Event.TenantId tenantId,
-            Types.Event.Type type,
+            String tenantId,
+            String type,
             Map<String, String> tags,
             Map<String, String> results,
-            Types.Event.Status status) {
+            String status) {
 
-        logger.info("Status - Send Event - Tenant: " + tenantId + ", Type: " + type + ", Status: " + status + ", Tags: " + tags + ", Results: " + results);
+        logger.info("Status - Send Event - Tenant: " + tenantId + ", Type: " +
+            type + ", Status: " + status + ", Tags: " + tags + ", Results: " + results);
 
         if (tenantId == null) {
             logger.error("No tenantId defined!");
@@ -152,51 +149,99 @@ public class StatusEventListener implements EventListener {
         String env = runner.getProps().get(STATUS_ENVIRONMENT);
         String host = "ec-platform-status-" + env + ".sfiqplatform.com";
         int port = 80;
+        String path = "/api/events/save";
 
-        if (tags == null) tags = Collections.emptyMap();
-        if (results == null) results = Collections.emptyMap();
+        JsonObject json = this.newEventJson(
+            type, status, tenantId, tags, results);
 
-        Types.Event.UUID id = Types.Event.UUID.newBuilder()
-                .setName(java.util.UUID.randomUUID().toString())
-                .build();
-
-        Instant time = Instant.now();
-        Timestamp timestamp = Timestamp.newBuilder()
-                .setSeconds(time.getEpochSecond())
-                .setNanos(time.getNano()).build();
-
-        Types.Event e1 = Types.Event.newBuilder()
-                .setId(id)
-                .setType(type)
-                .putAllTags(tags)
-                .setCreatedAt(timestamp)
-                .setStatus(status)
-                .putAllResults(results)
-                .setTenantId(tenantId)
-                .build();
-
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build();
-
-        StatusEventSenderGrpc.StatusEventSenderBlockingStub client = StatusEventSenderGrpc.newBlockingStub(channel);
-
-        Types.EventReceivedResponse response = client.saveSingleEvent(e1);
-        logger.info("Status Update - Response: " + response);
+        // TODO send REST POST call using org.apache.http.client.HttpClient
 
         return response;
     }
 
-    private Types.Event.TenantId getTenant(JobRunner runner)  {
-        if (!runner.getProps().containsKey(STATUS_TENANT)) return null;
-        return Types.Event.TenantId.newBuilder()
-                .setName(runner.getProps().get(STATUS_TENANT))
-                .build();
+    /**
+     * Private helper method to create event JsonObject
+     * For use in calling status service REST API
+     * 
+     * Event class (protobuf generated) looks like this:
+     * {
+     *   "id": {"name": "5ff38550-e976-4fb6-889a-46dcd508f6a8"},
+     *   "type": {"name": "modeling_job"},
+     *   "tags": {"app": "builder", "tenant": "test_org"},
+     *   "createdAt": "1970-01-01T00:00:00Z",
+     *   "status": "INITIALIZING",
+     *   "results": {"datasetId": "1"},
+     *   "tenantId": {"name": "tenant1"}
+     * }
+     * 
+     * @return JsonObject
+     */
+    private JsonObject newEventJson(
+            String type,
+            String status,
+            String tenantId,
+            Map<String, String> tags,
+            Map<String, String> results) {
+
+        JsonObject json = new JsonObject();
+
+        // Generate random UUID
+        String randomId = java.util.UUID.randomUUID().toString();
+        json.addProperty("id", withName(randomId));
+
+        // Current time in ISO-8601 representation
+        json.addProperty("createdAt", Instant.now().toString());
+
+        json.addProperty("type", withName(type));
+
+        json.addProperty("status", status);
+
+        json.addProperty("tenantId", withName(tenantId));
+
+        // Convert java Hashmap to String and add to JsonObject
+        Gson gson = new GsonBuilder().create();
+
+        json.addProperty("tags", gson.toJson(tags));
+
+        json.addProperty("results", gson.toJson(results));
+
+        return json;
     }
 
-    private Types.Event.Type getType(JobRunner runner)  {
+    // Helper method for generating json string
+    private static String withName(String s) {
+        return String.format("{\"name\": \"%s\"}", s)
+    }
+
+    /*
+     * Private helper methods for getting Azkaban properties
+     */
+    private String getTenant(JobRunner runner)  {
+        if (!runner.getProps().containsKey(STATUS_TENANT)) return null;
+        return runner.getProps().get(STATUS_TENANT);
+    }
+    private String getType(JobRunner runner)  {
         if (!runner.getProps().containsKey(STATUS_TYPE)) return null;
-        return Types.Event.Type.newBuilder()
-                .setName(runner.getProps().get(STATUS_TYPE))
-                .build();
+        return runner.getProps().get(STATUS_TYPE);
+    }
+    public Map<String, String> getTags(Logger logger, JobRunner runner) {
+        Map<String, String> tags = new HashMap<String, String>();
+
+        Props azkProps = runner.getProps();
+
+        if (azkProps != null) {
+            for (String key : azkProps.getKeySet()) {
+                String value = azkProps.get(key);
+                if (key.startsWith("status.tag.")) {
+                    String tag = key.substring("status.tag.".length());
+                    tags.put(tag, value);
+                } else {
+                    tags.put("azkaban.props." + key, value);
+                }
+            }
+        }
+
+        return tags;
     }
 
     /**
